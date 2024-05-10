9dd0ccb66f29efc3d225419a2c8c4af2d5319843
Non wiz <nonwiz@pm.me>
2024-05-10 10:43:54 +0700

    chore: add starter log

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,35 @@
 import { createConfig } from "express-zod-api";
+const bunny = `         
+                /|      __
+               / |   ,-~ /
+              Y :|  //  /
+              | jj /( .^
+              >-"~"-v"
+             /       Y
+            jo  o    |
+           ( ~T~     j
+            >._-' _./
+           /   "~"  |
+          Y     _,  |
+         /| ;-"~ _  l
+        / l/ ,-"~    \\
+        \\//\\/      .- \\
+         Y        /    Y    - ExpressJS - Boilerplate #1
+         l       I     !
+         ]\\      _\\    /"\\
+        (" ~----( ~   Y.  )
+        
+        `;
 
 export const config = createConfig({
     server: {
         listen: 8090, // port, UNIX socket or options
+        beforeRouting: ({ app, logger }) => {
+            console.clear();
+            console.log(bunny)
+            logger.info("Serving the API documentation at https://example.com/docs");
+            // app.use("/docs", ui.serve, ui.setup(documentation));
+        },
     },
     cors: true,
     logger: { level: "debug", color: true },
