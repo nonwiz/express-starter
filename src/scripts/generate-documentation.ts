bff129a59a074ef4f537495a20d0fd3803ad1493
Non wiz <nonwiz@pm.me>
2024-06-01 15:51:42 +0700

    fix: temporary disable gen docs

--- src/scripts/generate-documentation.ts
+++ src/scripts/generate-documentation.ts
@@ -1,23 +1,4 @@
-import {Documentation} from "express-zod-api";
-import {config, env} from "@/config";
-import {routing} from "@/routes";
-import {writeFile} from "node:fs/promises";
-import {formatDateToYYMM} from "@/utils/formatter";
-
-const docs = new Documentation({
-    routing,
-    config,
-    version: formatDateToYYMM(new Date()),
-    title: `${env.APP_NAME} API`,
-    serverUrl: `${env.BASE_URL}/api/`,
-})
-
 const generateDocumentation = async () => {
-    // return writeFile(
-    //     env.DOC_PATH,
-    //     ,
-    //     "utf-8",
-    // )
 }
 
 void generateDocumentation().then(_ => {
