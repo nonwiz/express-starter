504fb60d1d64c202c23790c7f1377dcfd6985ea7
Non wiz <nonwiz@pm.me>
2024-06-01 16:45:03 +0700

    test: allow cors

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,13 +1,15 @@
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 import {attachRouting} from "express-zod-api";
 import {routing} from "@/routes";
 
 export const app = express();
+app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json);
 export const { logger } = attachRouting({
     app,
     cors: true,
