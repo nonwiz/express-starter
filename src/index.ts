88356658c0e4b514f80f97c0b2fb393603cbdb6b
Non wiz <nonwiz@pm.me>
2024-06-01 16:16:04 +0700

    test: vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { createServer } from "express-zod-api";
 import {config} from "./config";
 import {routing} from "./routes";
 
-createServer(config, routing);
+export default createServer(config, routing);
