f0943b4dff59f01a1283cc318a9b83aa34f98ffa
Non wiz <nonwiz@pm.me>
2024-06-01 18:27:25 +0700

    test: add compression

--- src/config/express.ts
+++ src/config/express.ts
@@ -4,8 +4,12 @@ import express from "express";
 import cors from "cors";
 import {attachRouting} from "express-zod-api";
 import {routing} from "@/routes";
+import compression from "compression";
 
 export const app = express();
+
+app.use(compression());
+app.use(helmet());
 app.options('*', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -14,11 +18,11 @@ app.options('*', (req, res) => {
 });
 
 app.use(cors());
-app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 export const { logger } = attachRouting({
     app,
+    compression: true,
     cors: true,
     startupLogo: false,
     logger: { level: "debug", color: true }}, routing);
