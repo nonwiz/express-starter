d1cbf7a7211d6fa69a72389eb9ad1708ef2e8286
Non wiz <nonwiz@pm.me>
2024-06-01 14:49:47 +0700

    fix: temporary disable document

--- src/scripts/generate-documentation.ts
+++ src/scripts/generate-documentation.ts
@@ -4,18 +4,20 @@ import {routing} from "@/routes";
 import {writeFile} from "node:fs/promises";
 import {formatDateToYYMM} from "@/utils/formatter";
 
+const docs = new Documentation({
+    routing,
+    config,
+    version: formatDateToYYMM(new Date()),
+    title: `${env.APP_NAME} API`,
+    serverUrl: `${env.BASE_URL}/api/`,
+})
+
 const generateDocumentation = async () => {
-    return writeFile(
-        env.DOC_PATH,
-        new Documentation({
-            routing,
-            config,
-            version: formatDateToYYMM(new Date()),
-            title: `${env.APP_NAME} API`,
-            serverUrl: `${env.BASE_URL}/api/`,
-        }).getSpecAsYaml(),
-        "utf-8",
-    )
+    // return writeFile(
+    //     env.DOC_PATH,
+    //     ,
+    //     "utf-8",
+    // )
 }
 
 void generateDocumentation().then(_ => {
