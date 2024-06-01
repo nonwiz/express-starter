9b6acaa4e3b9c058620ade6bcb0b265bd0c2a78c
Non wiz <nonwiz@pm.me>
2024-06-01 20:54:22 +0700

    feat: add generate docs workaround

--- src/scripts/generate-documentation.ts
+++ src/scripts/generate-documentation.ts
@@ -1,4 +1,29 @@
-const generateDocumentation = async () => {
+import {createConfig, Documentation} from "express-zod-api";
+import { writeFile } from "node:fs/promises";
+import {env} from "@/config";
+import {routing} from "@/routes";
+
+const config = createConfig({
+    startupLogo: false,
+    server: {
+        listen: env.PORT
+    },
+    cors: true,
+    logger: { level: "debug", color: true },
+});
+
+export const generateDocumentation = async () => {
+    return writeFile(
+        "documentation/endpoints.yaml",
+        new Documentation({
+            routing,
+            config,
+            version: "1",
+            title: "Example API",
+            serverUrl: "https://example.com", // @ts-ignore
+        }).getSpecAsYaml(),
+        "utf-8",
+    )
 }
 
 void generateDocumentation().then(_ => {
